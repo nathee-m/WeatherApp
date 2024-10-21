d86aa03e88690aec94eaeefac36efd4b04f69d23
Natalia <83708330+nathee-m@users.noreply.github.com>
2024-10-21 15:32:16 -0300

    add mapsFragment

---
+++ app/src/main/java/com/natalia/myapplication/MapsFragment.java
@@ -0,0 +1,24 @@
+package com.natalia.myapplication;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MapsFragment extends Fragment {
+
+
+    public MapsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_maps, container, false);
+        return v;
+    }
+}
