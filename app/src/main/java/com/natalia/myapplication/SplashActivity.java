9f65f1b64c74d244c19570546bb326c799a35022
Natalia <83708330+nathee-m@users.noreply.github.com>
2024-10-21 09:17:56 -0300

    update files name

--- app/src/main/java/com/natalia/myapplication/SplashActivity.java
+++ app/src/main/java/com/natalia/myapplication/SplashActivity.java
@@ -5,18 +5,18 @@ import android.os.Bundle;
 import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     private static final int SPLASH_DURATION = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_splash);
 
 
         new Handler().postDelayed(() -> {
-            Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
             startActivity(intent);
             finish();
         }, SPLASH_DURATION);
