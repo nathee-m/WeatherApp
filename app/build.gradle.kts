b9a210d0064044eb321b8a3bc97208c7377e462f
Natalia <83708330+nathee-m@users.noreply.github.com>
2024-10-22 17:24:08 -0300

    implement osmdroid

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.activity)
     testImplementation(libs.junit)
+    implementation ("org.osmdroid:osmdroid-android:6.1.20")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     androidTestImplementation(libs.ext.junit)
