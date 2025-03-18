e8b3f122ae4b478f22e0203e2c08466f06ce0b4f
Natalia <83708330+nathee-m@users.noreply.github.com>
2025-03-18 17:54:46 -0300

    update DadosTempoTest

--- app/src/androidTest/java/com/natalia/myapplication/DadosTempoTest.java
+++ app/src/androidTest/java/com/natalia/myapplication/DadosTempoTest.java
@@ -3,6 +3,8 @@ package com.natalia.myapplication;
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
@@ -14,6 +16,7 @@ import org.junit.runner.RunWith;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -24,6 +27,12 @@ public class DadosTempoTest {
             new ActivityTestRule<>(TabsActivity.class);
 
     @Test
+    public void testWeatherImageLoading() {
+        ViewInteraction weatherImageView = Espresso.onView(withId(R.id.imageNow));
+        weatherImageView.check(matches(isDisplayed()));
+    }
+
+    @Test
     public void testWeatherDataDisplay() {
         Espresso.onView(withId(R.id.cityText)).check(matches(isDisplayed()));
         Espresso.onView(withId(R.id.tempNow)).check(matches(isDisplayed()));
@@ -32,16 +41,22 @@ public class DadosTempoTest {
 
     @Test
     public void testWeatherDataWeekDisplay() {
-        Espresso.onView(withId(R.id.dateText)).check(matches(isDisplayed()));
-        Espresso.onView(withId(R.id.minText)).check(matches(isDisplayed()));
-        Espresso.onView(withId(R.id.maxText)).check(matches(isDisplayed()));
-        Espresso.onView(withId(R.id.card02)).check(ViewAssertions.matches(isDisplayed()));
-    }
 
-    @Test
-    public void testWeatherImageLoading() {
-        ViewInteraction weatherImageView = Espresso.onView(withId(R.id.imageNow));
-        weatherImageView.check(matches(isDisplayed()));
-    }
+        Espresso.onView(withId(R.id.my_recycler_view)).check(matches(ViewMatchers.isDisplayed()));
 
+
+        String[] day = {"02/11", "03/11", "04/11", "05/11", "06/11", "07/11", "08/11"};
+        String[] weekDay = {"Seg", "Ter", "Qua", "Qui", "Sex", "Sáb", "Dom"};
+        int[] minTemps = {21, 24, 18, 19, 23, 27, 29};
+        int[] maxTemps = {35, 33, 25, 27, 32, 36, 35};
+
+        for (int i = 0; i < 7; i++) {
+            Espresso.onView(withId(R.id.my_recycler_view)).perform(RecyclerViewActions.scrollToPosition(i));
+
+            Espresso.onView(withText(day[i] + " - " + weekDay[i])).check(matches(ViewMatchers.isDisplayed()));
+            Espresso.onView(withText(minTemps[i] + "º")).check(matches(ViewMatchers.isDisplayed()));
+            Espresso.onView(withText(maxTemps[i] + "º")).check(matches(ViewMatchers.isDisplayed()));
+            Espresso.onView(withId(R.id.card02)).check(matches(isDisplayed()));
+        }
+    }
 }
