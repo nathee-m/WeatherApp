f68f82dd9a12bf4ab6348180ae8d25f4ad116284
Natalia <83708330+nathee-m@users.noreply.github.com>
2025-03-18 14:29:00 -0300

    create SobreActivityTest

---
+++ app/src/androidTest/java/com/natalia/myapplication/SobreActivityTest.java
@@ -0,0 +1,38 @@
+package com.natalia.myapplication;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.assertion.ViewAssertions;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class SobreActivityTest {
+
+    @Rule
+    public ActivityTestRule<SobreActivity> activityRule =
+            new ActivityTestRule<>(SobreActivity.class);
+
+    @Test
+    public void testMenuNavigationToSobreActivity() {
+        Espresso.onView(withId(R.id.toolbar)).perform(ViewActions.click());
+        Espresso.onView(withId(R.id.about)).check(ViewAssertions.matches(isDisplayed()));
+    }
+
+
+    @Test
+    public void testButtonSobreActivity() {
+        Espresso.onView(withId(R.id.fab2)).perform(ViewActions.click());
+        Espresso.onView(withId(R.id.home)).check(ViewAssertions.matches(isDisplayed()));
+    }
+}
