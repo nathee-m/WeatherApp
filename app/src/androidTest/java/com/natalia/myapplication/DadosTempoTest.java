d12dc9255af53522675209aaccd2219110730cda
Natalia <83708330+nathee-m@users.noreply.github.com>
2025-03-18 21:34:08 -0300

    update DadosTempoTest

--- app/src/androidTest/java/com/natalia/myapplication/DadosTempoTest.java
+++ app/src/androidTest/java/com/natalia/myapplication/DadosTempoTest.java
@@ -2,9 +2,7 @@ package com.natalia.myapplication;
 
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.ViewInteraction;
-import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.contrib.RecyclerViewActions;
-import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
@@ -26,6 +24,7 @@ public class DadosTempoTest {
     public ActivityTestRule<TabsActivity> activityRule =
             new ActivityTestRule<>(TabsActivity.class);
 
+
     @Test
     public void testWeatherImageLoading() {
         ViewInteraction weatherImageView = Espresso.onView(withId(R.id.imageNow));
@@ -41,22 +40,13 @@ public class DadosTempoTest {
 
     @Test
     public void testWeatherDataWeekDisplay() {
-
-        Espresso.onView(withId(R.id.my_recycler_view)).check(matches(ViewMatchers.isDisplayed()));
-
-
-        String[] day = {"02/11", "03/11", "04/11", "05/11", "06/11", "07/11", "08/11"};
-        String[] weekDay = {"Seg", "Ter", "Qua", "Qui", "Sex", "Sáb", "Dom"};
-        int[] minTemps = {21, 24, 18, 19, 23, 27, 29};
-        int[] maxTemps = {35, 33, 25, 27, 32, 36, 35};
+        Espresso.onView(withId(R.id.my_recycler_view)).check(matches(isDisplayed()));
 
         for (int i = 0; i < 7; i++) {
             Espresso.onView(withId(R.id.my_recycler_view)).perform(RecyclerViewActions.scrollToPosition(i));
-
-            Espresso.onView(withText(day[i] + " - " + weekDay[i])).check(matches(ViewMatchers.isDisplayed()));
-            Espresso.onView(withText(minTemps[i] + "º")).check(matches(ViewMatchers.isDisplayed()));
-            Espresso.onView(withText(maxTemps[i] + "º")).check(matches(ViewMatchers.isDisplayed()));
-            Espresso.onView(withId(R.id.card02)).check(matches(isDisplayed()));
+            Espresso.onView(withId(R.id.dateText)).check(matches(isDisplayed()));
+            Espresso.onView(withId(R.id.minText)).check(matches(isDisplayed()));
+            Espresso.onView(withId(R.id.maxText)).check(matches(isDisplayed()));
         }
     }
 }
